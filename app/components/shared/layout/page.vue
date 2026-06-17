06876c77d83107689e83144eb9a32262844533d1
ibednov <ibednov@live.ru>
2026-06-18 00:33:10 +0300

    feat (shared/layout): add slot for header-right in page layout component

--- app/components/shared/layout/page.vue
+++ app/components/shared/layout/page.vue
@@ -1,14 +1,17 @@
 <script setup lang='ts'>
-    //
+//
 </script>
 
 <template>
-   <div class="flex flex-col gap-4">
-    <shared-layout-header>
-        <slot name="header" />
-    </shared-layout-header>
-    <shared-layout-container>
-        <slot />
-    </shared-layout-container>
-   </div>
+    <div class="flex flex-col gap-4">
+        <shared-layout-header>
+            <slot name="header" />
+            <template #right>
+                <slot name="header-right" />
+            </template>
+        </shared-layout-header>
+        <shared-layout-container>
+            <slot />
+        </shared-layout-container>
+    </div>
 </template>
