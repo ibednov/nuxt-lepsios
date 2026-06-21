5b70b7d0646003aad096119fd1c98758f2a47842
ibednov <ibednov@live.ru>
2026-06-21 16:02:13 +0300

    feat (app/components/shared/layout): enhance header component with slot support for customizable left and right sections

--- app/components/shared/layout/header.vue
+++ app/components/shared/layout/header.vue
@@ -1,10 +1,15 @@
 <template>
     <div class="bg-lepsios-background p-4 rounded-b-xl flex justify-between items-center">
-        <div class=" text-2xl font-semibold">
+        <div v-if="$slots.left">
+            <slot name="left" />
+        </div>
+        <div class="text-2xl font-semibold flex items-center gap-2">
             <slot />
         </div>
-
-        <div class="flex items-center gap-2">
+        <div
+            v-if="$slots.right"
+            class="flex items-center gap-2"
+        >
             <slot name="right" />
         </div>
     </div>
--- app/components/shared/layout/page.vue
+++ app/components/shared/layout/page.vue
@@ -1,10 +1,9 @@
-<script setup lang='ts'>
-//
-</script>
-
 <template>
     <div class="flex flex-col gap-4">
         <shared-layout-header>
+            <template #left>
+                <slot name="header-left" />
+            </template>
             <slot name="header" />
             <template #right>
                 <slot name="header-right" />
