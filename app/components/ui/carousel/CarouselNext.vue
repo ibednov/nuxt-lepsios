8beb089a50fe5b344a9c730aa900d98723f8b0da
ibednov <ibednov@live.ru>
2026-09-18 21:21:40 +0300

    feat (i18n): carousel slide labels

--- app/components/ui/carousel/CarouselNext.vue
+++ app/components/ui/carousel/CarouselNext.vue
@@ -23,7 +23,7 @@ const { orientation, canScrollNext, scrollNext } = useCarousel()
     >
         <slot>
             <Icon name="lucide:arrow-right" class="h-4 w-4 text-current" />
-            <span class="sr-only">Next Slide</span>
+            <span class="sr-only">{{ $t('shared.ui.next_slide') }}</span>
         </slot>
     </Button>
 </template>
--- app/components/ui/carousel/CarouselPrevious.vue
+++ app/components/ui/carousel/CarouselPrevious.vue
@@ -23,7 +23,7 @@ const { orientation, canScrollPrev, scrollPrev } = useCarousel()
     >
         <slot>
             <Icon name="lucide:arrow-left" class="h-4 w-4 text-current" />
-            <span class="sr-only">Previous Slide</span>
+            <span class="sr-only">{{ $t('shared.ui.previous_slide') }}</span>
         </slot>
     </Button>
 </template>
