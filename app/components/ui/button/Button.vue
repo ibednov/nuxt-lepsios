02379a4aa53d1ab7f13ae73fe19b152ddcead90a
ibednov <ibednov@live.ru>
2026-09-23 15:23:53 +0300

    fix: expose default button variant attributes

--- app/components/ui/button/Button.vue
+++ app/components/ui/button/Button.vue
@@ -24,8 +24,8 @@ const props = withDefaults(defineProps<Props>(), {
 <template>
     <Primitive
         data-slot="button"
-        :data-variant="variant"
-        :data-size="size"
+        :data-variant="variant ?? 'default'"
+        :data-size="size ?? 'default'"
         :as="as"
         :as-child="asChild"
         :disabled="props.disabled"
